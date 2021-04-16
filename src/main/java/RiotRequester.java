89c00c77c47a6ad81a8a4c70d352238268a9d58b
6len <glen.cloughley@gmail.com>
2021-04-16 13:01:22 +0100

    update apikey

--- src/main/java/RiotRequester.java
+++ src/main/java/RiotRequester.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class RiotRequester {
     private OkHttpClient client = new OkHttpClient().newBuilder().build();
     private ObjectMapper objectMapper = new ObjectMapper();
-    private String riotApiKey = "RGAPI-c4f142f8-e448-4e91-ab7c-2db8ceead005";
+    private String riotApiKey = "RGAPI-a0641b74-8d89-4d91-bdc4-d486a715e4df";
 
     public String getAll(String summonerName) throws IOException {
         SummonerDTO summonerDTO = getSummoner(summonerName);
